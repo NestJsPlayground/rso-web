e51f342f9c2e8f51e8e3904d7eb1f161559a665e
Tilen Tomakić <tilen@tomakic.com>
2018-01-07 11:24:27 +0100

    Add @google-cloud/logging-winston

--- src/modules/logger/logger.service.ts
+++ src/modules/logger/logger.service.ts
@@ -5,10 +5,13 @@ import { environment } from '../../environment';
 @Component()
 export class LoggerService {
   constructor() {
+    const LoggingWinston = require('@google-cloud/logging-winston');
+
     require('winston-loggly-bulk');
 
     if (environment.loggly.token) {
       winston.add(winston.transports.Loggly, environment.loggly);
+      winston.add(LoggingWinston);
     }
   }
 }
